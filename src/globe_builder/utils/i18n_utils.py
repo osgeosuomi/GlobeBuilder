f4ca1ddadb5ce4b477468fa0c6ad34ac9dc5824e
Joonalai <joona.laine@cofactor.fi>
2026-09-20 20:14:28 +0300

    chore: update copyright headers

--- src/globe_builder/utils/i18n_utils.py
+++ src/globe_builder/utils/i18n_utils.py
@@ -1,3 +1,21 @@
+#  Copyright (C) 2026 GlobeBuilder contributors.
+#
+#
+#  This file is part of GlobeBuilder.
+#
+#  GlobeBuilder is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  GlobeBuilder is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with GlobeBuilder.  If not, see <https://www.gnu.org/licenses/>.
+
 from qgis.PyQt import QtCore
 from qgis_plugin_tools.tools import i18n
 
